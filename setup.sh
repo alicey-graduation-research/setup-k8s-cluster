ccdae195c317d002ae4957e4d9ea663763b0e52c
Alicey <sirius@alicey.dev>
2022-05-16 16:20:12 +0900

    update setup.sh multi-arch

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ apt-get update && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
 
