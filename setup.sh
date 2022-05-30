0219b98db5fe3ead96767b4923ac93a8a4aebee5
Alicey <sirius@alicey.dev>
2022-05-30 13:07:44 +0900

    update: setup.sh add sleep

--- setup.sh
+++ setup.sh
@@ -82,6 +82,7 @@ systemctl restart kubelet
 
 # コントロールプレーンの動作
 kubeadm init
+sleep 10
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
