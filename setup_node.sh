c7a2089c934679b100a94fb16c0706adf7b3c29e
Alicey <sirius@alicey.dev>
2022-11-21 01:33:14 +0900

    update k8s-session-observer

---
+++ setup_node.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu
+
+# change dir
+cd `dirname $0`
+
+# setup worker
+mkdir -p /opt/k8s-session-observer/
+cp ./$(dpkg --print-architecture)_node_join.o /opt/k8s-session-observer/k8s-session-observer.o
+chmod u+x /opt/k8s-session-observer/k8s-session-observer.o
+
+sudo tee /etc/systemd/system/k8s-session-observer.service <<EOF
+[Unit]
+Description=k8s-session-observer
+After=network-online.target
+[Service]
+ExecStart=/opt/k8s-session-observer/notification_token.o
+WorkingDirectory=/opt/k8s-session-observer/
+Restart=always
+User=$(id -u -n)
+Group=$(groups | cut -d ' ' -f 1)
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable k8s-session-observer.service
+sudo systemctl start k8s-session-observer.service
